d18c3524d8fad8d48b5d4a5f6c0f6a2d00ea98d8
Your Name <you@example.com>
2024-12-03 15:46:21 +0500

    pagenation fix

--- src/quiz/quiz.service.ts
+++ src/quiz/quiz.service.ts
@@ -101,9 +101,9 @@ export class QuizService {
     return {
       data: quizzes,
       pagination: {
+        total,
         page: pageNumber,
         limit: pageSize,
-        total,
         totalPages: Math.ceil(total / pageSize),
       },
     };
